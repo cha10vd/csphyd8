Problem with overlap checker for guest against its symmetry copies fixed with long radii array c.f. elegant broadcasting. To be fixed

csphyd8/automate.py
@@ -32,19 +32,19 @@ def gen_guests(structure, guest, trial_xyz, trial_rad):
     nVoids = len(voids)
     trials = np.array_split(trial_xyz, nVoids, axis=-1)
     for i, void in enumerate(voids):
-        trials[i]    = trials[i] + np.array(void])[:,None,None]
+        trials[i]    = trials[i] + np.array(void)[:,None,None]
     trials = np.dstack(trials)
     # 3. Check for host-guest overlap and filter results
-    hits      = check_overlap(sc_xyz, sc_rad, trials, trial_rad, 1.1)
-    print(hits)
-    trials    = filter_hits(trials, hits)
+    hits1  = check_overlap(sc_xyz, sc_rad, trials, 1.2)
+    trials    = filter_hits(trials, hits1)
     # 4. Generate symmetry related copies of accepted 
     #    guests and check guest-image overlap
     sc_xyz, sc_rad = make_images(trials, trial_rad, symm_ops)
-    hits      = check_overlap2(sc_xyz, sc_rad, trials, trial_rad, 1.1)
-    trials    = filter_hits(trials,hits)
+    hits2      = check_overlap2(sc_xyz, sc_rad, trials, 1.2)
+    hit_ids = [hits1[idx] for idx in hits2]
+    trials    = filter_hits(trials,hits2)
 
-    return structure, expansion, trials
+    return structure, expansion, trials, hits2#hit_ids
 
 def insert_guests(structure, trials):
     crystals =  make_cryst(structure, guest, trials)
@@ -75,6 +75,7 @@ if __name__ == '__main__':
 
 
     for structure in structures[5:6]:
-        guests = gen_guests(structure, guest, trial_xyz, trial_rad)
-    make_res_file(guests)
+        structure, expansion, guests, ids = gen_guests(structure, guest, trial_xyz, trial_rad)
+    crystals = make_cryst(structure, guest, guests, ids)
+    make_res_file(crystals)
     #structures = minimize_structures()

csphyd8/check_overlap_slow.py
@@ -39,13 +39,17 @@ def check_overlap2(host_xyz, host_rad, guest_xyz, scale):
 
     nh = 0
 
+    thresh_rad = host_rad * scale
+
     for trial in range(ntrial):
         overlap = 0
         for at in range(nat):
             dist_vecs = np.subtract(host_xyz[:,:,trial], guest_xyz[:,at,trial][:,None])
-            threshold = host_rad[:,at] * scale
+            threshold = thresh_rad[:,at]
             distances = np.subtract(np.linalg.norm(dist_vecs, axis=0),\
                                         threshold)
+            #distances = np.subtract(np.linalg.norm(dist_vecs, axis=0).reshape((3,-1)),\
+            #        threshold[:,None]).reshape(-1)
             min_dist  = np.min(distances)
             if min_dist < 0.0:
                 overlap = 1
@@ -54,6 +58,8 @@ def check_overlap2(host_xyz, host_rad, guest_xyz, scale):
         if overlap == 0:
             hits.append(trial)
             nh += 1
+
+    print('Number of hits identified: {}'.format(nh))
 
     return hits
 

csphyd8/expander.py
@@ -34,7 +34,12 @@ def expand_cryst(cryst, vec=None):
 
     if v.voids != []:
         centroids = np.array(v.centroids)
-        centroids = cryst.lattice.to_cartesian(centroids)
+        centroids += (centroids < 0)*1
+        print("CENTROIDS TEXT: {}", centroids)
+        frac, whole = np.modf(centroids)
+        print("CENTROIDS NUMPY: {}", frac)
+        centroids = cryst.lattice.to_cartesian(frac)
+        #centroids = cryst.lattice.to_cartesian(centroids)
         return(centroids, v, expansion)
     else:
         return 0

csphyd8/perturbator.py
@@ -20,7 +20,7 @@ logger.addHandler(ch)
 #log_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
 
 
-def perturb_mol(mol: Molecule, ntrials: int=100,
+def perturb_mol(mol: Molecule, ntrials: int=1000,
                 labels: bool=False) -> np.ndarray:
     """Pseudo-random generation of displacement and rotation perturbations
     about the origin of space.

csphyd8/symmetry.py
@@ -15,11 +15,12 @@ def cartesian_symm_ops(cryst):
 
     images = []
 
-    translations = np.array(list(itertools.product([0,-1,1], repeat=3)))
+    translations = np.array(list(itertools.product([0,-3-2,-1,1,2,3], repeat=3)))
     translations = translations @ to_cart
 
     expanded = []
     for op in symm_ops:
+        print(op)
         for trans in translations:
             expanded.append((op[0], op[1] + trans))
 
@@ -31,8 +32,8 @@ def make_images(trials, radii, symm_ops):
     images = []
 
     bonds_dictionary = Molecule.bonds_dictionary
-    rad = [[bonds_dictionary[(host_at, guest_at)] for guest_at in
-                    ["O", "H", "H"]] for host at in ["O", "H", "H"]]
+    #rad = [[bonds_dictionary[(host_at, guest_at)] for guest_at in
+    #                ["O", "H", "H"]] for host_at in ["O", "H", "H"]]
 
     for op in symm_ops[:]:
         nat, nmol = trials.shape[1:]
@@ -43,6 +44,12 @@ def make_images(trials, radii, symm_ops):
         images.append(im)
 
     images = np.concatenate(images[1:], axis=1) # Skip identity op of mol.
+
+    rad = []
+    for mol in range(len(symm_ops)-1):
+        for atom in ["O","H","H"]:
+            rad.append([bonds_dictionary[(atom, l)] for l in \
+                                         ["O","H","H"]])
 
     radii  = np.array(rad)
     return images, radii